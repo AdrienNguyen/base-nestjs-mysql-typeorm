5df1cea70d303e157e7d544b9daaf967b03af381
AdrienNguyen <phuong.na163228@gmail.com>
2022-12-23 11:05:23 +0700

    update: Update health check api

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
+  SwaggerModule.setup('api/docs', app, document, {
     swaggerOptions: {
       tagsSorter: 'alpha',
       operationsSorter: 'alpha',
